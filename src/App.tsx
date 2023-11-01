c55878d31242dc114ad262311b48a92f756be0c1
noah <noah@powerarena.com>
2023-11-01 18:11:15 +0800

    fix🐛: sth error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import Logo from './assets/logo.svg'
+
+import { useSyncExternalStore } from "react";
+
+import {todosStore} from './todoStore'
 
 import './App.css';
 
 function App() {
+  const todos = useSyncExternalStore(todosStore.subscribe, todosStore.getSnapshot)
+
   return (
-    <div className="App" style={{backgroundColor: "#181818",height: '100vh'}}>
-      <Logo/>
+    <div>
+      <button onClick={() => {
+        todosStore.addTodo()
+      }}>Add Todo</button>
+      <hr />
+      <ul>
+        {todos.map(todo => (<li key={todo.id}>{todo.text}</li>))}
+      </ul>
     </div>
   );
 }
